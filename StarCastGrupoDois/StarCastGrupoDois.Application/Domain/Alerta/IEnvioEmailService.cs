2c203e4d4bbe91a5fcdb9844dbfbeeb1b8810194
Andre Luiz Bacaglini @ Cast Group <andre.bacaglini@castgroup.com.br>
2018-07-07 17:30:10 -0300

    add envioEmailService

---
+++ StarCastGrupoDois/StarCastGrupoDois.Application/Domain/Alerta/IEnvioEmailService.cs
@@ -0,0 +1,12 @@
+﻿using StarCastGrupoDois.Domain.Entities.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarCastGrupoDois.Application.Domain.Alerta
+{
+    public interface  IEnvioEmailService
+    {
+        void Enviar(string mensagem, List<Email> emails);
+    }
+}
--- StarCastGrupoDois/StarCastGrupoDois.Infra.Repository/Repository/DoadorRepositorio.cs
+++ StarCastGrupoDois/StarCastGrupoDois.Infra.Repository/Repository/DoadorRepositorio.cs
@@ -1,7 +1,11 @@
-﻿using StarCastGrupoDois.Application.Domain.Doador;
+﻿using System.Collections.Generic;
+using StarCastGrupoDois.Application.Domain.Doador;
 using StarCastGrupoDois.Application.Repository.Doador;
 using StarCastGrupoDois.Domain.Entities.Models;
+using StarCastGrupoDois.Domain.Entities.ValueObject;
 using StarCastGrupoDois.Infra.Repository.Context;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace StarCastGrupoDois.Infra.Repository.Repository
 {
@@ -9,6 +13,11 @@ namespace StarCastGrupoDois.Infra.Repository.Repository
     {
         public DoadorRepositorio(StarCastGroupoDoisContexto db) : base(db)
         {
+        }
+
+        public List<Email> GetAllEmails()
+        {
+           return DbSet.AsNoTracking().Select(x=>x.Email).ToList();
         }
     }
 }
