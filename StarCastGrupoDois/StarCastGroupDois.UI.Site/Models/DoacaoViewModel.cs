4dbcb7fb99dcb83c55392f2e8096930700b8e691
Eduardo Garcia <garciaedu1@hotmail.com>
2018-07-07 16:19:51 -0300

    add doacao

---
+++ StarCastGrupoDois/StarCastGroupDois.UI.Site/Models/DoacaoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using StarCastGrupoDois.Domain.Entities.Fixed;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using StarCastGrupoDois.Infra.CrossCutting.Messages;
+
+namespace StarCastGroupDois.UI.Site.Models
+{
+    public class DoacaoViewModel
+    {
+        [Required(ErrorMessage = Mensagem.CAMPO_OBRIGATORIO)]
+        public string DescricaoSangue { get; set; }
+        [Required(ErrorMessage = Mensagem.CAMPO_OBRIGATORIO)]
+        public string Descricao { get; set; }
+        [Required(ErrorMessage = Mensagem.CAMPO_OBRIGATORIO)]
+        public TipoDoacao TipoDoacao { get; set; }
+    }
+}
